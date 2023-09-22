09aca2337d4c3aaffac7c52f74d3c8cc8dd1afa8
Bharath14 <pythonopq77@gmail.com>
2023-09-22 07:59:48 +0530

    Updated QA assignment given on 15th Sep

--- Assignments/Assignment10/singleNumber.js
+++ Assignments/Assignment10/singleNumber.js
@@ -1,11 +1,40 @@
-function findSingleNumber(A) {
-  let result = 0;
-  for (let i = 0; i < A.length; i++) {
-    result ^= A[i];
+// function findSingleNumber(A) {
+//   let result = 0;
+//   for (let i = 0; i < A.length; i++) {
+//     result ^= A[i];
+//   }
+//   return result;
+// }
+
+// // Test cases
+// console.log(findSingleNumber([1, 2, 2, 3, 1, 2, 2, 8])); // Output: 3, 8
+// console.log(findSingleNumber([1, 2, 2, 4, 4, 6]));      // Output: 1, 6
+
+
+function findSingleNumbers(A) {
+  const frequencyMap = {};
+  const repeatedElements = [];
+
+  for (const num of A) {
+    if (frequencyMap[num]) {
+      frequencyMap[num]++;
+    } else {
+      frequencyMap[num] = 1;
+    }
   }
-  return result;
+
+  for (const num in frequencyMap) {
+    if (frequencyMap[num] === 1) {
+      repeatedElements.push({
+        value: parseInt(num),
+        frequency: frequencyMap[num]
+      });
+    }
+  }
+  return repeatedElements;
 }
 
 // Test cases
-console.log(findSingleNumber([1, 2, 2, 3, 1])); // Output: 3
-console.log(findSingleNumber([1, 2, 2]));      // Output: 1
+console.log(findSingleNumbers([1, 2, 2, 3, 1, 4, 2, 2, 8, 9])); // Output: [3, 8]
+console.log(findSingleNumbers([1, 2, 2, 4, 4, 6, 5]));      // Output: [1, 6]
+
